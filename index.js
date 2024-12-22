e582f3d532c943ac54a3bdccddaefcbbfe43fba7
Devin Xie <devinxie418@gmail.com>
2024-12-22 10:31:45 -0500

    added grid size feature, and color picker

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-let gridSize = 16;
-const pixelSize = 100/gridSize;
-const content = document.querySelector("#content");
+function displayGrid() {
+    const pixelSize = 100/gridSize;
+    const content = document.querySelector("#grid");
+    content.textContent = "";
 
-for(let i = 0; i < gridSize; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+    for(let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
 
-    for(let j = 0; j < gridSize; j++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixel.style.cssText = `width: ${pixelSize}%; height: 100%; border: solid 1px lightgray;`
-        row.appendChild(pixel);
+        for(let j = 0; j < gridSize; j++) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            pixel.style.cssText = `width: ${pixelSize}%; height: 100%;`
+            row.appendChild(pixel);
+        }
+        row.style.cssText= `display: flex; width: 100%; height: ${pixelSize}%`;
+        content.appendChild(row);
     }
-    row.style.cssText= `display: flex; width: 100%; height: ${pixelSize}%`;
-    content.appendChild(row);
+
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseover", () => {
+            let color = document.getElementById("favcolor").value;
+            pixel.style.background = `${color}`;
+        });
+    });
+
+    const reset = document.querySelector(".reset button");
+    reset.addEventListener("click", () => {
+        pixels.forEach((pixel) => {
+            pixel.style.background = "white";
+        });
+    });
 }
 
-const pixels = document.querySelectorAll(".pixel");
-pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", () => {
-        pixel.style.background = "blue";
-    });
-});
+const newGrid = document.querySelector("#newGrid");
+let gridSize = 32;
+displayGrid();
 
-const reset = document.querySelector(".reset button");
-reset.addEventListener("click", () => {
-    pixels.forEach((pixel) => {
-        pixel.style.background = "white";
-    });
-});
 
+newGrid.addEventListener("click", () => {
+    gridSize = prompt("Enter a grid size between 1-64");
+    while(gridSize < 1 || gridSize > 64) {
+        gridSize = prompt("Please keep the size between 1 and 64");
+    }
+
+    displayGrid();
+});
 
